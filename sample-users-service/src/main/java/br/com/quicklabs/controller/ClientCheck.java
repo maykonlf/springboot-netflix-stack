1d322c5b764e3abc3fc1bdc1de59287d6396b8ef
Maykon L Fonseca <maykonlfonseca@gmail.com>
2017-03-22 10:27:16 -0300

    Add Hystrix fallback use

--- sample-users-service/src/main/java/br/com/quicklabs/controller/ClientCheck.java
+++ sample-users-service/src/main/java/br/com/quicklabs/controller/ClientCheck.java
@@ -1,5 +1,6 @@
 package br.com.quicklabs.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Random;
 
 /**
  * Created by maykon on 22/03/17.
@@ -33,6 +36,20 @@ public class ClientCheck {
             result = result.concat("\n");
         }
         return result;
+    }
+
+    @RequestMapping(value = "/hystrix", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "fallbackHystrix")
+    public String hystrixCheck() throws Exception {
+        if (true) {
+            throw new Exception();
+        } else {
+            return "Something wrong isn't right with my code";
+        }
+    }
+
+    public String fallbackHystrix() {
+        return "Hystrix working \\o/\n";
     }
 
 }
